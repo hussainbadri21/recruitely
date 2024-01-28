a56b0e696353271f80cd79d8acd7ddbf1243f891
Hussain Calcuttawala <hussain.c@peopleinteractive.in>
2024-01-29 00:37:23 +0530

    Adds POST api

--- src/app/candidates/add/page.tsx
+++ src/app/candidates/add/page.tsx
@@ -1,31 +1,57 @@
 "use client"
 
 import React from 'react';
-import { Button, Select, Form, Input } from 'antd';
-
-const onFinish = (values: any) => {
-    console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
-
+import { Button, Select, Form, Input, Slider, message } from 'antd';
 
 
 type FieldType = {
-    name?: string;
-    password?: string;
-    remember?: string;
+    candidate_name?: string;
 };
 const { Option } = Select;
 
 const App: React.FC = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const onFinish = (values: any) => {
+        fetch('/candidates/add/submit', { method: "POST", body: JSON.stringify(values) })
+            .then(async res => {
+                const body = await res.json()
+                switch (res.status) {
+                    case 200:
+
+                        messageApi.open({
+                            type: 'success',
+                            content: body.message,
+                        });
+                        window.location.href = '/candidates'
+                        break;
+                    case 400:
+                        messageApi.open({
+                            type: 'error',
+                            content: body.message,
+                        });
+                        break;
+                }
+            })
+            .catch((e) => {
+                console.log(e)
+                messageApi.open({
+                    type: 'error',
+                    content: 'Something went wrong',
+                });
+            });
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+
     const prefixSelector = (
         <Form.Item name="prefix" noStyle>
             <Select style={{ width: 70 }}>
-                <Option value="86">+86</Option>
-                <Option value="87">+87</Option>
+                <Option value="91">+91</Option>
+                <Option value="1">+1</Option>
             </Select>
         </Form.Item>
     );
@@ -40,55 +66,78 @@ const App: React.FC = () => {
             range: '${label} must be between ${min} and ${max}',
         }
     };
-    return (
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            validateMessages={validateMessages}
-        >
-            <Form.Item<FieldType>
-                label="Candidate Name"
-                name="name"
-                rules={[{ required: true }]}
-            >
-                <Input />
-            </Form.Item>
+    const formatter = (value: number | undefined) => {
+        value = value || 0
+        let months = Math.round(value * 12)
+        return months >= 12 ?
+            `${parseInt((months / 12).toString())} years ${months % 12 > 0 ? `${(months % 12)} months` : ''}` :
+            `${months} months`
+    };
 
-            <Form.Item name={['user', 'email']} label="Email" rules={[{ required: true, type: 'email' }]}>
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="phone"
-                label="Phone Number"
-                rules={[{ required: true, min: 8, max: 15 }]}
+
+    return (
+        <>
+            {contextHolder}
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                validateMessages={validateMessages}
             >
-                <Input type='number' addonBefore={prefixSelector} style={{ width: '100%' }} />
-            </Form.Item>
-            <Form.Item name="salary" label="Expected Salary" rules={[{ required: true, type: "number" }]}>
-                <Input type='number' />
-            </Form.Item>
-            <Form.Item label="Current Status">
-                <Select>
-                    <Select.Option value="0">Contacted</Select.Option>
-                    <Select.Option value="1">Interview Scheduled</Select.Option>
-                    <Select.Option value="2">Offer Extended</Select.Option>
-                    <Select.Option value="3">Hired</Select.Option>
-                    <Select.Option value="4">Rejected</Select.Option>
-                </Select>
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form >
+                <Form.Item<FieldType>
+                    label="Candidate Name"
+                    name="candidate_name"
+                    rules={[{ required: true }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item name='email' label="Email"
+                    rules={[{ required: true, type: 'email' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="phone"
+                    label="Phone Number"
+                    rules={[{ required: true, min: 8, max: 15 }]}
+                >
+                    <Input type='number' addonBefore={prefixSelector} style={{ width: '100%' }} />
+                </Form.Item>
+                <Form.Item name={['skills', 'react']}
+                    label="Experience in React">
+                    <Slider step={0.1} min={0} max={40} tooltip={{ formatter }} />
+                </Form.Item>
+                <Form.Item name={['skills', 'node']}
+                    label="Experience in NodeJS">
+                    <Slider step={1 / 12} min={0} max={40} tooltip={{ formatter }} />
+                </Form.Item>
+                <Form.Item name="expected_salary" label="Expected Salary" rules={[{ required: true }]}>
+                    <Input type='number' />
+                </Form.Item>
+
+                <Form.Item name="current_status" label="Current Status" rules={[{ required: true }]}>
+                    <Select placeholder="Select current status">
+                        <Option value="0">Contacted</Option>
+                        <Option value="1">Interview Scheduled</Option>
+                        <Option value="2">Offer Extended</Option>
+                        <Option value="3">Hired</Option>
+                        <Option value="4">Rejected</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form >
+        </>
     )
 };
 
-export default App;
+export default App
--- src/app/candidates/fetch/route.ts
+++ src/app/candidates/fetch/route.ts
@@ -3,7 +3,7 @@ import prisma from '../../../../lib/prisma';
 
 export async function GET() {
 
-    const results = await prisma.candidate.findMany({ where: { s: 1 } })
+    const results = await prisma.candidate.findMany({ where: { active: 1 } })
 
     return Response.json({ candidates: results })
 }
